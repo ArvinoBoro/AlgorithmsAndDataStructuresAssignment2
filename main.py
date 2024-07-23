5ad27c0f7c2649fa2e2860e51e411ba096c75c04
ArvinoBoro <arvin.ziba@ontariotechu.net>
2024-07-23 12:13:14 -0400

    Added Display Functionality Additions and Changes - Each print statement is preceded by a conditional that is false if the user chooses not the display the algorithm steps. - Added conditional sleep() methods before each print() that execute on the condition that the user chooses to display the steps of the algorithm slowly. - The user also has the option to quickly display the steps of the algorithm, which results in the print conditional resolving to true and the sleep conditional resolving to false.

--- main.py
+++ main.py
@@ -4,65 +4,87 @@ from playsound import playsound
 underline_start = '\033[4m'
 underline_end = '\033[0m'
 
-def merge_sort(A, level=0):
-    print(f"\n{level * '  '}merge_sort({A})")
-    print(f"{level * '  '}A = {A}")
+def merge_sort(A, level, display, slow):
+    if slow: time.sleep(2)
+    if display: print(f"\n{level * '  '}merge_sort({A})")
+    if slow: time.sleep(2)
+    if display: print(f"{level * '  '}A = {A}")
 
     if len(A) < 2:
-        print(f"{level * '  '}Length of A < 2.")
+        if slow: time.sleep(2)
+        if display: print(f"{level * '  '}Length of A < 2.")
         return
-    print(f"{level * '  '}Length of A ≥ 2.")
+    if slow: time.sleep(2)
+    if display: print(f"{level * '  '}Length of A ≥ 2.")
     
-    m = len(A) // 2 
-    print(f"{level * '  '}Divide A as evenly as possible.")
+    m = len(A) // 2
+    if slow: time.sleep(2)
+    if display: print(f"{level * '  '}Divide A as evenly as possible.")
     a1 = A[:m]
-    print(f"{level * '  '}a1 = {a1}")
+    if slow: time.sleep(0.5)
+    if display: print(f"{level * '  '}a1 = {a1}")
     a2 = A[m:]
-    print(f"{level * '  '}a2 = {a2}")
+    if slow: time.sleep(0.5)
+    if display: print(f"{level * '  '}a2 = {a2}")
 
-    merge_sort(a1, level + 1)
-    merge_sort(a2, level + 1)
+    merge_sort(a1, level + 1, display, slow)
+    merge_sort(a2, level + 1, display, slow)
 
-    merge(A, a1, a2, level)
+    merge(A, a1, a2, level, display, slow)
 
-def merge(A, a1, a2, level):
-    print(f"\n{level * '  '}merge({A}, {a1}, {a2})")
-    print(f"{level * '  '}A = {A}")
-    print(f"{level * '  '}a1 = {a1}")
-    print(f"{level * '  '}a2 = {a2}")
+def merge(A, a1, a2, level, display, slow):
+    if slow: time.sleep(2)
+    if display: print(f"\n{level * '  '}merge({A}, {a1}, {a2})")
+    if slow: time.sleep(2)
+    if display: print(f"{level * '  '}A = {A}")
+    if slow: time.sleep(2)
+    if display: print(f"{level * '  '}a1 = {a1}")
+    if slow: time.sleep(2)
+    if display: print(f"{level * '  '}a2 = {a2}")
     i = 0
     j = 0 
     while i + j < len(A):
-        print(f"\n{level * '  '}i = {i}")
-        print(f"{level * '  '}j = {j}")
+        if slow: time.sleep(2)
+        if display: print(f"\n{level * '  '}i = {i}")
+        if slow: time.sleep(2)
+        if display: print(f"{level * '  '}j = {j}") 
         if j == len(a2):
-            print(f"{level * '  '}j = Length of a2")
+            if slow: time.sleep(2)
+            if display: print(f"{level * '  '}j = Length of a2")
             A[i+j] = a1[i]
             i += 1 
         elif i < len(a1) and a1[i] < a2[j]:
-            print(f"{level * '  '}j ≠ Length of a2")
-            print(f"{level * '  '}i < Length of a1")
-            print(f"{level * '  '}a1[i] < a2[j]")
+            if slow: time.sleep(2)
+            if display: print(f"{level * '  '}j ≠ Length of a2")
+            if slow: time.sleep(2)
+            if display: print(f"{level * '  '}i < Length of a1")
+            if slow: time.sleep(2)
+            if display: print(f"{level * '  '}a1[i] < a2[j]")
             A[i+j] = a1[i]
             i += 1
         else:
-            print(f"{level * '  '}j ≠ Length of a2")
+            if slow: time.sleep(2)
+            if display: print(f"{level * '  '}j ≠ Length of a2")
             if i >= len(a1):
-                print(f"{level * '  '}i ≥ Length of a1")
+                if slow: time.sleep(2)
+                if display: print(f"{level * '  '}i ≥ Length of a1")
             elif a1[i] >= a2[j]:
-                print(f"{level * '  '}a1[i] ≥ a2[j]")
+                if slow: time.sleep(2)
+                if display: print(f"{level * '  '}a1[i] ≥ a2[j]")
             A[i+j] = a2[j]
             j += 1 
         
-        print(f"{level * '  '}[", end='')
-        for k in range(len(A)):
-            if k == i + j - 1:
-                print(f"{underline_start}{A[k]}{underline_end}", end='')
-            else:
-                print(f"{A[k]}", end='')
-            if k < len(A) - 1:
-                print(", ", end='')
-        print("]")
+        if slow: time.sleep(2)
+        if display:
+            print(f"{level * '  '}[", end='')
+            for k in range(len(A)):
+                if k == i + j - 1:
+                    print(f"{underline_start}{A[k]}{underline_end}", end='')
+                else:
+                    print(f"{A[k]}", end='')
+                if k < len(A) - 1:
+                    print(", ", end='')
+            print("]")
 
 def main():
     print("Welcome to Arvin's merge sort visualizer!\n")
@@ -83,8 +105,23 @@ def main():
             i += 1    
         except ValueError:
             print("Error: Input must be an integer.")
+
+    print("\nDisplay algorithm steps?")
+    print("1. Yes, but fast")
+    print("2. Yes, but slow")        
+    print("0. No")
     
-    merge_sort(A)
+    while True:
+        try:
+            user_input = int(input("Select an option: "))
+            if user_input > -1 and user_input < 3:
+                break
+            else: 
+                print("Error: Input must be an integer from 0 to 2 inclusive.")
+        except ValueError:
+                print("Error: Input must be an integer.")
+
+    merge_sort(A, 0, user_input != 0, user_input == 2)
     print(f"\nSorted array: {A}")
 
 if __name__ == '__main__': 
